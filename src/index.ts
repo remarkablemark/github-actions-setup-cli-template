d1e02698f31ce910a53b239a5b4b3c3de7c73d86
Mark <mark@remarkablemark.org>
2024-02-24 11:59:51 -0500

    refactor: tidy index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
 import { addPath, getInput, setFailed } from '@actions/core';
 import { downloadTool, extractTar, extractZip } from '@actions/tool-cache';
-import path from 'path';
 
 import { getDownloadObject } from './utils';
 
 export async function run() {
   try {
-    // Get version of tool to be installed
+    // Get the version of the tool to be installed
     const version = getInput('cli-version');
 
-    // Download the specific version of the tool, e.g. as a tarball/zipball
+    // Download the specific version of the tool (e.g., tarball/zipball)
     const download = getDownloadObject(version);
     const pathToTarball = await downloadTool(download.url);
 
-    // Extract the tarball/zipball onto host runner
+    // Extract the tarball/zipball onto the host runner
     const extract = download.url.endsWith('.zip') ? extractZip : extractTar;
-    const pathToCLI = await extract(pathToTarball);
+    const binaryDirectory = await extract(pathToTarball);
 
     // Expose the tool by adding it to the PATH
-    addPath(path.join(pathToCLI, download.binPath));
+    addPath(binaryDirectory);
   } catch (error) {
     if (error instanceof Error) {
       setFailed(error.message);
