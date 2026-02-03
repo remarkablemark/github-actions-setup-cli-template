6069c7012b4b428f5cf930e181181901e2d9ae08
Mark <mark@remarkablemark.org>
2026-02-02 19:08:59 -0500

    chore(jest): tidy config
    https://kulshekhar.github.io/ts-jest/docs/getting-started/options/useESM

--- jest.config.ts
+++ jest.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
-    '^.+\\.m?ts$': ['ts-jest', { useESM: true }],
+    '^.+\\.m?[tj]sx?$': ['ts-jest', { useESM: true }],
   },
 };
 
