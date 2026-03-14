2b054d5ca26d91936b6bd68066ce158a59b42625
Mark <mark@remarkablemark.org>
2026-03-14 13:13:29 -0400

    test: remove describe block from integration test

--- src/index.integration.test.ts
+++ src/index.integration.test.ts
@@ -3,7 +3,7 @@ import { rm } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { afterAll, describe, expect, it } from 'vitest';
+import { afterAll, expect, it } from 'vitest';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 const nodePath = process.execPath;
@@ -20,26 +20,24 @@ afterAll(async () => {
   );
 });
 
-describe('integration', () => {
-  it('runs action', () => {
-    const result = spawnSync(nodePath, [actionPath], {
-      env: {
-        ...process.env,
-        'INPUT_CLI-NAME': 'gh',
-        'INPUT_CLI-VERSION': '2.27.0',
-        RUNNER_TOOL_CACHE: runnerToolCachePath,
-        RUNNER_TEMP: runnerTempPath,
-      },
-      stdio: 'pipe',
-    });
-
-    if (result.status !== 0) {
-      /* eslint-disable no-console */
-      console.error('stdout:', result.stdout?.toString());
-      console.error('stderr:', result.stderr?.toString());
-      /* eslint-enable no-console */
-    }
-
-    expect(result.status).toBe(0);
+it('runs action', () => {
+  const result = spawnSync(nodePath, [actionPath], {
+    env: {
+      ...process.env,
+      'INPUT_CLI-NAME': 'gh',
+      'INPUT_CLI-VERSION': '2.27.0',
+      RUNNER_TOOL_CACHE: runnerToolCachePath,
+      RUNNER_TEMP: runnerTempPath,
+    },
+    stdio: 'pipe',
   });
+
+  if (result.status !== 0) {
+    /* eslint-disable no-console */
+    console.error('stdout:', result.stdout?.toString());
+    console.error('stderr:', result.stderr?.toString());
+    /* eslint-enable no-console */
+  }
+
+  expect(result.status).toBe(0);
 });
